5d2ce5c949da240c69d23bde529fd8449cab3e1e
ed <deriaam@outlook.com>
2020-06-29 06:42:18 -0700

    add microsoft

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Ahmed from Seattle, WA");
+      System.out.println("Ahmed from Seattle, WA is near of Microsoft");
    }
 }
